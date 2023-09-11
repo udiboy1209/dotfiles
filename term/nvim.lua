d3e04184acca412a9dc5dabffe629ae4f2be7128
Meet Udeshi <mudeshi1209@gmail.com>
2023-09-11 12:38:28 -0400

    Add completion to neovim

--- term/nvim.lua
+++ term/nvim.lua
@@ -10,6 +10,9 @@ Plug('feline-nvim/feline.nvim')
 Plug('rightson/vim-p4-syntax')
 Plug('dcampos/nvim-snippy')
 Plug('duane9/nvim-rg')
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-telescope/telescope.nvim')
+Plug('neovim/nvim-lspconfig')
 
 vim.call('plug#end')
 
@@ -30,11 +33,15 @@ vim.opt.shiftwidth = 4
 
 -- Leader shortcuts
 vim.keymap.set('n', '<Leader><cr>', ':nohl<cr>')
-vim.keymap.set('', '<Leader><Leader>', '<Esc>')
+vim.keymap.set('!', '<Leader><Leader>', '<Esc>')
 vim.keymap.set('x', '<Leader>y', '"+y')
 vim.keymap.set('x', '<Leader>p', '"+p')
 vim.keymap.set('n', '<Leader>i', ':bprev<cr>')
 vim.keymap.set('n', '<Leader>o', ':bnext<cr>')
+vim.keymap.set('n', '<Leader>cn', ':cn<cr>')
+vim.keymap.set('n', '<Leader>cp', ':cp<cr>')
+vim.keymap.set('n', '<Leader>cl', ':ccl<cr>')
+vim.keymap.set('n', '<Leader>cw', ':cw<cr>')
 
 -- OneDark colorscheme
 require('onedark').setup {
@@ -76,3 +83,45 @@ require('snippy').setup({
         },
     },
 })
+
+-- Setup language server
+require('lspconfig').jedi_language_server.setup({})
+
+vim.keymap.set('n', '<Leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<Leader>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gk', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', 'gK', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<Leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<Leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<Leader>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<Leader>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<Leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<Leader>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
+
+vim.opt.completeopt = 'menu'
+vim.keymap.set('i', '<C-O>', '<C-X><C-O>')
